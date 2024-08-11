4bc7112280f1eaf5681fd266123afa7648b97ac1
leejuhyeon <stallysora143@daum.net>
2024-08-11 21:28:42 +0900

    char study gets();, puts();

--- Clangg/charSample.c
+++ Clangg/charSample.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	char szName[32] = { 0 };
 	printf("이름을 입력하세요.: ");
-	gets(szName); // gets()는 보안 결함 문제가 있다.
+	gets(szName); // gets()는 보안 결함 문제가 있음
 
 	printf("당신의 이름은 ");
 	puts(szName);
